0909af36457eecb8544aa2136e72fc2c5c59ef8c
Andre Setiawan <setiawanandre06@gmail.com>
2026-05-05 14:51:20 +0700

    feat: initialize project dependencies and configure development environment settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,15 +1,20 @@
 from .base import *
+from decouple import config
 
-SECRET_KEY = "django-insecure-ganti-ini-dengan-string-acak-untuk-development"
+SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Sementara pakai SQLite dulu, nanti ganti PostgreSQL
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
     }
 }
